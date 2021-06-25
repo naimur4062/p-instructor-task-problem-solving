56dd44fcdaf4d5abf7a0d538f0d536ba64995dba
naimur4062 <naimur4062@gmail.com>
2021-06-26 00:09:35 +0800

    problem-solver

---
+++ problem-solving.js
@@ -0,0 +1,21 @@
+const factorial = n => {
+    if (n <= 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+const maxNumber = (array, integer) => {
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        const number = array[i] / integer;
+        const factorialNumber = factorial(number);
+        if (factorialNumber > max) {
+            max = factorialNumber;
+        }
+    }
+    return max;
+}
+
+console.log(maxNumber([1, 4, 7, 16], 4));
